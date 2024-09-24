65ddd483241730ee4ae636e14ae61739c87e0f2d
Rudransh Upreti <126793635+rudranshupreti@users.noreply.github.com>
2024-09-25 00:00:35 +0530

    pash

--- basic.js
+++ basic.js
@@ -200,3 +200,28 @@ console.log(typeof null)        // object
 // parseInt()
 
 
+// // && ampersand operator example
+
+// const check = 4 > 3 && 10 > 5         // true && true -> true
+// const check = 4 > 3 && 10 < 5         // true && false -> false
+// const check = 4 < 3 && 10 < 5         // false && false -> false
+
+// // || pipe or operator, example
+
+// const check = 4 > 3 || 10 > 5         // true  || true -> true
+// const check = 4 > 3 || 10 < 5         // true  || false -> true
+// const check = 4 < 3 || 10 < 5         // false || false -> false
+
+// //! Negation examples
+
+// let check = 4 > 3                     // true
+// let check = !(4 > 3)                  //  false
+// let isLightOn = true
+// let isLightOff = !isLightOn           // false
+// let isMarried = !false                // true
+
+// Ternary Operators
+let isRaining = true
+isRaining
+  ? console.log('You need a rain coat.')
+  : console.log('No need for a rain coat.')
