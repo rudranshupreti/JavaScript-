48cf87a2aad47ba3b5b4fa721cb33e8fec727000
rudranshupreti17@gmail.com <rudranshupreti7@gmail.com>
2024-09-28 17:51:20 +0530

    inital commit

--- basic.js
+++ basic.js
@@ -1,33 +1,82 @@
-const slot = [
-    { date: "2024-09-11", isBooked: true, _id: 231132 },
-    { date: "2024-09-11", isBooked: true, _id: 231131 },
-    { date: "2024-09-11", isBooked: true, _id: 231133 },
-    { date: "2024-09-12", isBooked: true, _id: 231134 },
-    { date: "2024-09-12", isBooked: true, _id: 231135 },
-    { date: "2024-09-12", isBooked: true, _id: 231136 },
+const questionAnswer = [
+    {
+      id: 1,
+      question: "Coastal State of India?",
+      optionA: "Maharashtra",
+      optionB: "Haryana",
+      optionC: "UP",
+      optionD: "Kerala",
+      answer: ["a", "b"],
+    },
+    {
+      id: 2,
+      question: "Asian Country?",
+      optionA: "India",
+      optionB: "China",
+      optionC: "USA",
+      optionD: "Russia",
+      answer: ["a", "b"],
+    },
+    {
+      id: 3,
+      question: "Capital city of J&K?",
+      optionA: "Leh",
+      optionB: "Jammu",
+      optionC: "Kargil",
+      optionD: "Shree Nagar",
+      answer: ["b", "d"],
+    },
+    {
+      id: 4,
+      question: "City in Europe?",
+      optionA: "London",
+      optionB: "Biging",
+      optionC: "Delhi",
+      optionD: "Ankara",
+      answer: ["a"],
+    },
   ];
   
-
-function groupslotByDate (slots){
-    const groupSlots = {};
-    slot.forEach((slot) => {
-        if(!groupSlots[slot.date]){
-            groupSlots[slot.date] = [];
-        }
-        groupSlots[slot.date].push({
-            isBooked: slot.isBooked,
-            _id: slot._id,
-    });
-    
-        
-    });
-    const result = Object.keys(groupSlots).map((date) => ({
-        date: date,
-        slots: groupSlots[date],
-      }));
-
-      return result;
+  const userAnswer = [
+    {
+      questionId: 1,
+      answer: ["c", "b"],
+    },
+    {
+      questionId: 3,
+      answer: ["b", "c"],
+    },
+    {
+      questionId: 4,
+      answer: ["a"],
+    },
+  ];
+  
+  const result = {
+    skip: 1,
+    wrong: 2,
+    right: 1,
+    totalAttempt: 3,
+  };
+  
+questionAnswer.forEach((question) => {
+    const userAns = userAnswer.find((ua) => ua.questionId === question.id);
+    if(!userAns){
+        result.skip++;
+    }
+    else{
+        const iscorrect = userAns.answer.length === question.answer.length &&
+        userAns.answer.every((val) => question.answer.include(val));
+    if (iscorrect){
+        result.right++;
+    }else{
+        result.wrong++
+    }
 }
 
-const result  = groupslotByDate(slot);
-console.log(result)
+    
+    
+});
+
+
+return result;
